c14aeb0e54e204d8b1be75500805cf2f02326d2e
sim manseop <tlqaksqhr@naver.com>
2020-02-28 23:08:01 +0900

    add url pattern

--- django_sudoku/sudoku/urls.py
+++ django_sudoku/sudoku/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('/sudoku/check', views.check name='sudoku_check'),
+    path('/sudoku/make', views.make_sudoku, name='sudoku_make'),
 ]
