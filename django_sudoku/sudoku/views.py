c537a7abe1b356e3ecb6e924efd0eeb2696f6a31
sim manseop <tlqaksqhr@naver.com>
2020-02-28 23:04:21 +0900

    add controllers

--- django_sudoku/sudoku/views.py
+++ django_sudoku/sudoku/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views import templates
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Index Page!")
+
+def check(request):
+    return HttpResponse("Check Page!")
+
+def make_sudoku(request):
+    return HttpResponse("Sudoku Make Page!")
