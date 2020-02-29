70c6ff5fc4fdb184c869d7617badf8a1d8554fa7
sim manseop <tlqaksqhr@naver.com>
2020-02-29 20:09:12 +0900

    add code to use sessions

--- django_sudoku/sudoku/views.py
+++ django_sudoku/sudoku/views.py
@@ -6,15 +6,32 @@ from .api.sudoku import Sudoku
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Index Page!")
+    context = {}
+    return render(request, 'sudoku/index.html',context)
+
+def register_ranking(request):
+    pass
 
 def check_sudoku(request):
-    return HttpResponse("Check Page!")
+    sudoku_api = Sudoku()
+    puzzle = request.POST['puzzle']
+    elapsed_time = request.POST['time']
+
+    result = sudoku_api.sudoku_check(puzzle)
+    data = {}
+    if result == True:
+        data['status'] = "clear"
+        request.session['status'] = "clear"
+        request.session['elapsed_time'] = elapsed_time
+    else:
+        data['status'] = "fail"
+
+    return JsonResponse(data)
 
 def make_sudoku(request):
 
     sudoku_api = Sudoku()
-    board = sudoku_api.generate_puzzle()
+    board = sudoku_api.generate_sudoku()
     result = {
         'board' : board
     }
