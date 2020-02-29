77362080fcd126ed750d84f58f71cd37d92d456b
sim manseop <tlqaksqhr@naver.com>
2020-02-29 14:13:37 +0900

    add calling api code, and fix view name

--- django_sudoku/sudoku/views.py
+++ django_sudoku/sudoku/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views import templates
+from .api.sudoku import Sudoku
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Index Page!")
 
-def check(request):
+def check_sudoku(request):
     return HttpResponse("Check Page!")
 
 def make_sudoku(request):
-    return HttpResponse("Sudoku Make Page!")
+
+    sudoku_api = Sudoku()
+    board = sudoku_api.generate_puzzle()
+    result = {
+        'board' : board
+    }
+
+    return JsonResponse(result)
