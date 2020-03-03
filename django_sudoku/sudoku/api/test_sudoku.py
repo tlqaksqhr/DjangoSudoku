b49a2b6656d3fb605dd6f210d4df299be6fdac8b
sim manseop <tlqaksqhr@naver.com>
2020-03-03 20:43:05 +0900

    add test case

--- django_sudoku/sudoku/api/test_sudoku.py
+++ django_sudoku/sudoku/api/test_sudoku.py
@@ -1,7 +1,7 @@
-from sudoku import Sudoku
+from .sudokus import Sudoku
 
 def test_check_method_if_true():
-    sudoku = Sudoku()
+    sudokus = Sudoku()
     true_board = [
         [4,3,5,2,6,9,7,8,1],
         [6,8,2,5,7,1,4,9,3],
@@ -13,10 +13,10 @@ def test_check_method_if_true():
         [2,4,8,9,5,7,1,3,6],
         [7,6,3,4,1,8,2,5,9]
     ]
-    assert sudoku.sudoku_check(true_board) == True
+    assert sudokus.sudoku_check(true_board) == True
 
 def test_check_method_if_false():
-    sudoku = Sudoku()
+    sudokus = Sudoku()
     false_board = [
         [7,3,5,2,6,9,7,8,1],
         [6,8,2,5,7,1,4,9,3],
@@ -28,34 +28,34 @@ def test_check_method_if_false():
         [2,4,8,9,5,7,1,3,6],
         [7,6,3,4,1,8,2,5,9]
     ]
-    assert sudoku.sudoku_check(false_board) == False
+    assert sudokus.sudoku_check(false_board) == False
 
 def test_invalid_size_check():
-    sudoku = Sudoku()
+    sudokus = Sudoku()
     invalid_board = [
         [7,3,5,2,6,9,7,8,1],
         [6,8,2,5,7,1,4,9,3],
         [1,9,7,8,3,4,5,6,2]
     ]
-    assert sudoku.sudoku_check(invalid_board) == False
+    assert sudokus.sudoku_check(invalid_board) == False
 
 def test_invalid_board_int():
-    sudoku = Sudoku()
+    sudokus = Sudoku()
     invalid_board = 1
-    assert sudoku.sudoku_check(invalid_board) == False
+    assert sudokus.sudoku_check(invalid_board) == False
   
 def test_invalid_board_empty_list():
-    sudoku = Sudoku()
+    sudokus = Sudoku()
     invalid_board = []
-    assert sudoku.sudoku_check(invalid_board) == False
+    assert sudokus.sudoku_check(invalid_board) == False
     
 def test_invalid_board_float():
-    sudoku = Sudoku()
+    sudokus = Sudoku()
     invalid_board = 0.1
-    assert sudoku.sudoku_check(invalid_board) == False
+    assert sudokus.sudoku_check(invalid_board) == False
     
 def test_invalid_board_element_not_int():
-    sudoku = Sudoku()
+    sudokus = Sudoku()
     invalid_board = [
       [1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0],
       [1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0],
@@ -67,4 +67,4 @@ def test_invalid_board_element_not_int():
       [1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0],
       [1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0]
     ]
-    assert sudoku.sudoku_check(invalid_board) == False 
+    assert sudokus.sudoku_check(invalid_board) == False 
